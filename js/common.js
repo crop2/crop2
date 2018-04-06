0b2fa04262322312f26196631886bbfcd5bf3ae6
xswx@yandex.ru <s0ftplus73>
2018-04-06 14:10:26 +0400

    апп
    ап

---
+++ js/common.js
@@ -0,0 +1,15 @@
+$(function(){
+   $("#callButton").click(function(){
+   		$(".Bottomlayer").show(1000);
+   });
+
+    $("#btnClose").click(function(){
+   		$(".Bottomlayer").hide(1000);
+   });
+
+  /*  $("#callButton").animate*/
+
+
+
+});
+   
